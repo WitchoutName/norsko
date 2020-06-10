bab321051c17539a8727590e4d73b0e2216be732
Fomáš <46920928+WitchoutName@users.noreply.github.com>
2020-06-11 01:29:30 +0200

    Add files via upload

---
+++ js/geograife.js
@@ -0,0 +1,306 @@
+let places = [
+    {
+        "name":"Drive the Atlanterhavsveien (The Atlantic Road)",
+        "description":"The Atlanterhavsveien, or commonly known as the Atlantic Road in English is a gorgeous and impressive drive through one of the best places in Norway. Connecting the mainland of Norway with the pretty island of Averøya, it’s a great place to drive for the day. Make sure to visit Kvernes Stave Church, too. It’s around 600-years old and one of the oldest in the region. Make sure to keep your eyes peeled for Storseisundet Bridge, too. 🚙",
+        "id":"hand"
+    },
+    {
+        "name":"See the beautiful Hamnøy",
+        "description":"Hamnøy in Lofoten has become known, the world over, for its impressive landscape and unspoilt charm… and it’s easy to see why. Possibly one of the best places in Norway – it’s a spot you should definitely head to without hesitation.",
+        "id":"hamony"
+    },
+    {
+        "name":"Visit the tiny village of Reine",
+        "description":"Easily, one of the best places in Norway – Reine is picture perfect. Nestled on the island of Moskenesøya it is actually part of the Lofoten set of islands and archipelago that’s made this region so gorgeous. For a stunning experience, wrap up warm (with plenty of layers) and head to Bunes Beach, especially at night. Find a nice spot, lay back on the sandy beach and watch the dancing Northern Lights above you. It really is a special experience.",
+        "id":"renee"
+    },
+    {
+        "name":"Explore the Arctic Circle on a Spa Boat",
+        "description":"Yup, you head that right! There’s a dedicated spa boat in the Arctic Circle that you can hop on and discover more of the best  places in Norway.  Moored in Tromso, it’s best to book your tickets online and arrive in plenty of time before the boat leaves.",
+        "id":"spaboat"
+    },
+    {
+        "name":"Hike Pulpit Rock",
+        "description":"You’re almost guaranteed one of the best views of Norway from the top of Pulpit Rock. I mean, it ’s one of the best places in Norway – especially with the vistas you’ll see across the fjord. Now, the hike itself (return) took around 4-5 hours but it really depends on how fast and fit you are.",
+        "id":"pulpid"
+    },
+    {
+        "name":"Spot puffins on Runde Island",
+        "description":"Runde Island is one of the prettiest and best places in Norway to visit – especially for a tiny little island. You can actually sail directly on a RIB boat from the city of Alesund, which is great if you’re short on time.",
+        "id":"runde"
+    },
+    {
+        "name":"Visit the Briksdalsbreen Glacier",
+        "description":"Possibly one of the easiest glaciers to visit around Stryn, the Briksdalsbreen Glacier is truly one of the best places in Norway you should see. The Glacier itself is actually an ‘offshoot’ or arm of the much larger, Jostedalsbreen glacier which is also amazing to see (but harder to get to). So if you’re looking for a challenge, maybe look for another route/glacier.",
+        "id":"glacier"
+    },
+    {
+        "name":"See the Geirangerfjord",
+        "description":"There are many beautiful fjords in Norway. They’re a dime-a-dozen but some just really stand out, don’t they? For me, that’s Geirangerfjord. Make sure get to the highest viewpoint at Dalsnibba, it’ll take you around 40-minutes to drive but well worth it. Just make sure to pack some thick coats… it gets chilly when you’re this high up.",
+        "id":"geiran"
+    },
+    {
+        "name":"Drive the Trollstigen",
+        "description":"Between the villages of Åndalsnes and Valldal is the gorgeous Trollstigen drive. Possibly, the most well-known road in Norway, it really is a stunning route to drive.",
+        "id":"trollstigen"
+    },
+    {
+        "name":"See Norway’s National Mountain, Stetind",
+        "description":"Around 15 miles, or so, from Kjøpsvik – Stetind Mountain is Norway’s national mountain. It’s look is quite different to many other mountains in the area, so you can’t miss it. est of all, if you’re feeling energetic or fancy a challenge, try climbing to the top with a professional guide. Narvik Guides are probably one of the best in the area with all their guides being IFMGA Mountain Guide certified.",
+        "id":"stetind"
+    },
+    {
+        "name":"Hike to Trolltunga",
+        "description":"The hike itself is pretty demanding and not for the faint-hearted and you should really consider if your fit enough before heading along the trail. The whole hike will take all day (if not, 12+ hours if you’re a little slower), so make sure you pack lots of supplies and dress responsibly. No one likes a reckless hiker.",
+        "id":"trolltunga"
+    }
+]
+
+let cities = [
+    {
+        "name":"Haugesund",
+        "description": "Once an important fishing port, it is now the oil industry that fuels Haugesund and keeps it moving. A lively place, the waterfront area around the port has some interesting bars and restaurants which are worth checking out. As the area has been inhabited for thousands of years, there are loads of interesting historical sights to visit, with the ancient church and viking farm being particularly noteworthy. When visiting Haugesund, a great thing to do is take a boat trip to the nearby Karmoy island – a picturesque place to explore.",
+        "id":"haugesund"
+    },
+    {
+        "name":"Tonsberg",
+        "description": "Although relatively few remnants of the past remain, Tonsberg is actually the oldest town in the whole of Norway. As such, history lovers will enjoy stopping by when traveling from Oslo along the coast, to see all that it has to offer. There is a crumbling old castle, some Viking ruins and graves, as well as a cool museum with the skeleton of a blue whale in it. A lively town, the landscapes surrounding Tonsberg are also nice, if people fancy taking a trip into the nearby countryside.",
+        "id":"tonsberg"
+    },
+    {
+        "name":"Drammen",
+        "description": "Traditionally an industrial center with a bustling port and a reputation as a grimy, grey and grim city, Drammen has cleaned up a lot in recent years, and as a result, is a lot more tourist friendly. Lying not too far from Oslo, the city has some lovely walks and trails on both sides of the river that splits Drammen in two, and the center of town is easily traversed on foot. Visitors nearly always use this city as a stepping stone to the interior of the country, or as a stop-off on the way to the nearby mines at Blafarvevaerket.",
+        "id":"drammen"
+    },
+    {
+        "name":"Larvik",
+        "description": "Lying on the southern coast of Norway, Larvik has a busy port and, although it is not a very popular tourist destination, there is more than enough to warrant a visit. There are a couple of fantastic historical museums in town and a new cultural center has recently opened up, while the old baroque lighthouse looks impressively out over the sea. From here you can visit the country´s largest beech forest at Bokeskogen, and the surrounding area has some great viking excavations for visitors to enjoy.",
+        "id":"larvik"
+    },
+    {
+        "name":"Lillehammer",
+        "description": "Having hosted the Winter Olympics in 1994, it is unsurprising that Lillehammer has such a fantastic array of winter sport activities on offer. Lying on the edge of Lake Mjosa, there are some great museums and galleries about town, as well as a number of lovely restaurants. One of the most popular ski resorts in the country, Lillehammer has a picturesque setting, with roving hills and forests all around it. Although there are interesting things to see and do throughout the year, winter is when Lillehammer really comes alive.",
+        "id":"lillehammer"
+    },
+    {
+        "name":"Hamar",
+        "description": "Set on the banks of the largest lake in Norway, Hamar has a surprising amount to do given its size. It’s not a bad option if you are looking to stop off somewhere when traveling to the north from Oslo. It has the largest glass building in Europe, which remarkably has the ruins of a cathedral inside. The accompanying museum about the vikings is captivating. Apart from this, Hamar is a charming, laid-back place where you can also head out for some fishing and kayaking on the lake.",
+        "id":"hamar"
+    },
+    {
+        "name":"Kristiansand",
+        "description": "The fifth largest city in Norway, Kristiansand is a charming place with a lovely marina and lively ambiance. With lots of shopping options and some great restaurants and bars along the waterfront, the city has some pretty sandy beaches and claims to be Norway´s most popular holiday resort. While Norwegians do indeed head here in large numbers, it is often to stop by on the way to other destinations in the south. The nearby archipelago is lovely to explore and the southern coast is not too far away either.",
+        "id":"kristiansand"
+    },
+    {
+        "name":"Fredrikstad",
+        "description": "Lying on the banks of the Glomma river, Fredrikstad is a lovely, old fortified city that has been very well-preserved. A picturesque place to wander around – when the sun is shining, the modern waterfront is particularly delightful; there are a number of coffee shops, restaurants and bars for visitors to enjoy. On the opposite bank, the old town, with its impressive Kongsten fort and distinctive moat, is the undoubted highlight, and is what makes Fredrikstad a popular haunt for tourists.",
+        "id":"fredrikstad"
+    },
+    {
+        "name":"Bodo",
+        "description": "As the largest city in the Nordic region, Bodo is an important commercial center and transport hub to the area around it. Although the city itself is uninspiring, architecturally speaking -it was almost completely destroyed in the Second World War – Bodo´s beautiful location, with snow-capped peaks off in the distance, makes up for its drab buildings. Situated at the end of the incredible Kystriksveien Coastal Route, many people visit Bodo to get to the mesmerizing Lofoten Islands nearby. From here you can explore the wild and rugged north of the country – that in itself makes Bodo worth visiting.",
+        "id":"bodo"
+    },
+    {
+        "name":"Tromso",
+        "description": "Located in the far north of Norway, Tromso is set on an island amidst lovely blue fjords and spectacular snow-capped mountains. One of the northernmost places that you can visit in Europe, the city actually lies around 350-kilometres north of the Arctic Circle. As such, it is one of the better sites in Norway from which to view the Northern Lights. With loads of pubs on offer, a healthy cultural scene and lively nightlife, visiting Tromso can be great fun. There are lots of winter sports available and loads of scenic landscapes in which to pursue them. Tour organizers in the city can arrange trips to the Arctic if you´re after an adventure.",
+        "id":"tromso"
+    },
+    {
+        "name":"Stavanger",
+        "description": "A dynamic place, Stavanger´s economy is booming due to the nearby oil fields. As such, the dreaded urban sprawl has taken over much of the outskirts of the city. Accompanying this has been an upsurge in prices, and it is now one of the most expensive places in the country. The center of Stavanger has managed to avoid being engulfed by modern developments and is full of old wooden buildings which are very pretty to walk around. The nearby waterfront is very lively during the summer and there are lots of great restaurants to choose from, as well as some lively nightlife. Many people stop by Stavanger on their way to the fantastic sights at Lysefjorden and Preikestolen.",
+        "id":"stavanger"
+    },
+    {
+        "name":"Alesund",
+        "description": "A slightly bizarre place to visit, due to its eclectic mix of architectural styles, the city was destroyed by a fire in 1904. Its hasty reconstruction accounts for the mix of mock-Gothic, Art Nouveau and folkloric embellishments which you find when wandering its streets. Built on a few islands lying next to each other, the glittering harbor adds to Alesund´s attractiveness. From the nearby hills, there are some breathtaking panoramic views of the surrounding fjords and mountains. A lively city, Alesund has lots of magnificent restaurants and a thriving bar scene.",
+        "id":"alesund"
+    },
+    {
+        "name":"Trondheim",
+        "description": "One of the most picturesque cities in the whole country, Trondheim is a pleasure to walk around, as forest-clad hills and glistening waterways surround its colorful buildings and sweet, old harbor. The historic capital of Norway has an amazing medieval cathedral. There is a sense of timelessness about its laid-back streets, as people leisurely go about their lives. There´s more than enough to keep you entertained for a few days, and you´ll soon discover its fine museums, great restaurants and atmospheric cafes. Alongside its rich cultural heritage, there is a contemporary arts and music scene, due to the large university campus. After Trondheim, head north to explore all the wild landscapes that northern Norway has to offer.",
+        "id":"trondheim"
+    },
+    {
+        "name":"Bergen",
+        "description": "Formerly a part of the Haneseatic league and once the capital of Norway, Bergen sits in a spectacular location and visitors will love the plethora of sights on offer. With seven hills surrounding it, as well as seven fjords, the city´s brightly-colored buildings tumble down the slopes until they reach Bryggen – the fantastic wooden houses at the city center which were once used for trading and commerce. With some great art museums, a lively music scene and upbeat nightlife, Bergen has a nice atmosphere to it – although it unfortunately rains nearly every day of the year. Hiking in the surrounding mountains is glorious, and taking a boat ride amidst the nearby fjords offers up some stunning panoramas.",
+        "id":"bergen"
+    },
+    {
+        "name":"Oslo",
+        "description": "The capital of the country is full of amazing architectural designs that highlight the contemporary feel about the place, as do the educational museums, interesting galleries and evocative art pieces. Lying next to the sea, with mountains surrounding it, Oslo is one of the greenest cities in the world, thanks to its forward-thinking eco-friendly policies – this makes it lovely to walk around. In actual fact, residents can find themselves skiing, trekking the forests, or sailing along the Oslo fjord’s waterways in no time at all. A lively place, Oslo has a fantastic gastronomic scene as well as a raucous nightlife with lots of trendy bars and nightclubs.",
+        "id":"oslo"
+    },
+    
+]
+
+let populations = [
+    {
+        "region":"Finnmark",
+        "population": 1.6
+    },
+    {
+        "region":"Nordland",
+        "population": 6.7
+    },
+    {
+        "region":"Troms",
+        "population": 6.6
+    },
+    {
+        "region":"Møre og Romsdal",
+        "population": 18.1
+    },
+    {
+        "region":"Sør-Trøndelag",
+        "population": 17.5
+    },
+    {
+        "region":"Aust-Agder",
+        "population": 13.6
+    },
+    {
+        "region":"Hordaland",
+        "population": 35
+    },
+    {
+        "region":"Rogaland",
+        "population": 53.3
+    },
+    {
+        "region":"Sogn og Fjordane",
+        "population": 6.1
+    },
+    {
+        "region":"Vest-Agder",
+        "population": 26.6
+    },
+    {
+        "region":"Akershus",
+        "population": 129.3
+    },
+    {
+        "region":"Buskerud",
+        "population": 19.8
+    },
+    {
+        "region":"Hedmark",
+        "population": 7.4
+    },
+    {
+        "region":"Oppland",
+        "population": 7.9
+    },
+    {
+        "region":"Oslo",
+        "population": 1532.7
+    },
+    {
+        "region":"Telemark",
+        "population": 12.2
+    },
+    {
+        "region":"Vestfold",
+        "population": 114.7
+    },
+    {
+        "region":"Østfold",
+        "population": 74.4
+    },
+    {
+        "region":"Nord-Trøndelag",
+        "population": 6.5
+    }
+]
+let markerPath = [
+    ["m 150.57358,660.00018 c 5.0516,0 5.89095,3.59574 5.57315,6.56195 -0.31779,2.96621 -5.57315,11.05644 -5.57315,11.05644 0,0 -4.8849,-7.83598 -5.39338,-11.05644 -0.50848,-3.22046 0.34178,-6.56195 5.39338,-6.56195 z", "matrix(0.06165618,0,0,0.06165618,147.53342,662.13705)"],
+    ["m 150.55553,654.26103 c 6.69714,0 7.80991,4.76705 7.38859,8.6995 -0.42131,3.93244 -7.38859,14.65804 -7.38859,14.65804 0,0 -6.47615,-10.38853 -7.15026,-14.65804 -0.67412,-4.26952 0.45311,-8.6995 7.15026,-8.6995 z","matrix(0.0853586,0,0,0.0853586,146.3486,657.43929)"]
+    ]
+
+
+
+$(function(){
+    let regionsLink = [];
+    let placesLink = [];
+    let citiesLink = [];
+    function init(){
+        document.getElementById("map").innerHTML = mapSource.map;
+        $("#map svg").css("width","100%");
+        $("#map svg").css("height","auto");
+        $("#map svg").attr("viewBox","0 0 619 734");
+
+        for(let path of $("#map path")){
+            let reg = populations.find(region => {return region.region == $(path).attr("name")});
+            if(reg != null){
+                regionsLink.push([path, reg]);
+            }
+        }
+
+        for(let marker of $("#map g")){
+            let place = places.find(mem => {return mem.id == $(marker).attr("id")});
+            if(place != null){
+                placesLink.push([marker, place]);
+            }
+        }
+        for(let marker of $("#map g")){
+            let city = cities.find(mem => {return mem.id == $(marker).attr("id")});
+            if(city != null){
+                placesLink.push([marker, city]);
+            }
+        }
+    }
+
+    let buttons = Array.from(document.getElementById("secnav").children)
+    $(buttons[0]).on("click", function(){
+        init()
+        let popVal = [2, 4, 10, 20, 40, 80, 160, 2000]
+        let colVal = [70, 80, 110, 130, 150, 170, 190, 255]
+        let box = Array.from($("#map rect")).find(mem => {return $(mem).attr("id") == "textBg"});
+        let text = Array.from($("#map text")).find(mem => {return $(mem).attr("id") == "textText"});
+        
+        function popToCol(color){
+            for(let i = 0; i < popVal.length; i++){
+                if(color < popVal[i])
+                return colVal[i]
+            }
+            return null;
+        }
+
+        for(let i = 0; i < colVal.length; i++){
+            $(Array.from($("#map rect"))[i+2]).css("fill", `rgb(0, ${colVal[i]}, 0)`)
+        }
+
+        for(let region of regionsLink){
+            console.log(region)
+            let greenValue = popToCol($(region[1]).attr("population"));
+            let color = `rgb(0, ${greenValue}, 0)`;
+            $(region[0]).css("fill", color);
+            $(region[0]).on("mouseover", function(){
+                box.style.fill = color;
+                box.style.stroke = "black"
+                if(greenValue < 111)
+                    text.style.fill = "white";
+                else
+                    text.style.fill = "black"
+                
+                text.children[0].innerHTML = `Region: ${region[1].region}`;
+                text.children[1].innerHTML = `Population: ${region[1].population} P/km²`;
+            })
+        }
+            
+        for(let marker of placesLink.join(citiesLink)){
+            $(marker[0]).on("mouseover", function(){
+                console.log($(marker[1]).attr("name"))
+            })
+        }
+
+    })
+
+    $(buttons[1]).on("click", function(){
+        init()
+    })
+    
+    
+
+})
